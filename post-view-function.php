5459902a30f45c4a31c485d27f2c6852f463abb0
BNIX VN <164770398+bnixvn@users.noreply.github.com>
2024-04-01 23:03:36 +0700

    Create post-view-function.php

---
+++ post-view-function.php
@@ -0,0 +1,41 @@
+<?php
+
+//CODE LAY LUOT XEM
+function getPostViews($postID){
+	$count_key = 'post_views_count';
+	$count = get_post_meta($postID, $count_key, true);
+	if($count==''){
+		delete_post_meta($postID, $count_key);
+		add_post_meta($postID, $count_key, '0');
+		return "<b>01 </b>";
+	}
+	return $count.' ';
+}
+
+// CODE DEM LUOT XEM
+function setPostViews($postID) {
+	$count_key = 'post_views_count';
+	$count = get_post_meta($postID, $count_key, true);
+	if($count==''){
+		$count = 0;
+		delete_post_meta($postID, $count_key);
+		add_post_meta($postID, $count_key, '0');
+	}
+	else {
+		$count++;
+		update_post_meta($postID, $count_key, $count);
+	}
+}
+
+//CODE HIEN THI SO LUOT XEM BAI VIET TRONG DASHBOARDH
+add_filter('manage_posts_columns', 'posts_column_views');
+add_action('manage_posts_custom_column', 'posts_custom_column_views',5,2);
+function posts_column_views($defaults){
+	$defaults['post_views'] = __('Lượt xem');
+	return $defaults;
+}
+function posts_custom_column_views($column_name, $id){
+	if($column_name === 'post_views'){
+		echo getPostViews(get_the_ID());
+	}
+}
